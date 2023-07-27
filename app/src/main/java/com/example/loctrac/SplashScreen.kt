ca3579e48d9ba300540b1ffeda3385c0366411c2
Apoorv <apoorvsharma0708@gmail.com>
2023-07-28 00:55:51 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/loctrac/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.loctrac
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+
+
+    }
+}
